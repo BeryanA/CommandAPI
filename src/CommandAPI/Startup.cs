c49dd6cd74803b53f9bf6e015a7f786635d2e75a
Bas <asberyan@gmail.com>
2020-12-08 16:42:37 +0400

    add migration

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -10,13 +10,21 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace CommandAPI
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+       
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<CommandContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
@@ -24,7 +32,7 @@ namespace CommandAPI
             services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+       
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
